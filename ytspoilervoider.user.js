f3e22f8a84b2682d63f3c983b7f59f0c3f8326f4
Peter Hieu Vu <peter.vu8@gmail.com>
2025-02-27 00:09:30 -0800

    update classes to hide, add ability to hide upcoming stream titles

--- ytspoilervoider.user.js
+++ ytspoilervoider.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTubeSpoilerVoider
 // @license      GPLv3
-// @version      0.1.4
+// @version      0.2.0
 // @description  Hide youtube video durations
 // @author       peterhieuvu
 // @match        https://*.youtube.com/*
@@ -16,10 +16,13 @@
 
     const STYLED_KEY = 'ytspoilervoider-applied';
     const DECORATED_KEY = 'ytspoilervoider-decorated';
+    const OBFUSCATED_KEY = 'ytspoilervoider-obfuscated';
+
+    // TODO: make this configurable
+    const CONF_KEEP_TIME_IN_URL = true; // store current video time in URL when seeking
+    const CONF_HIDE_UPCOMING_STREAMS = true; // hide upcoming stream titles and thumbnails in live tab
 
     const applyStyles = () => {
-        console.log('applying ytspoilervoider styles')
-
         const style = document.createElement('style');
         const classesToHide = [
             // player classes
@@ -27,12 +30,25 @@
             '.ytp-time-separator',
             '.ytp-time-duration',
             '.ytd-thumbnail-overlay-time-status-renderer', // watch page side thumbnail times
+            '.ytInlinePlayerControlsTimeDisplay',
             '.YtInlinePlayerControlsTimeDisplay', // discover page time display
             '.video-time',
         ];
         style.innerText = `${classesToHide.join(', ')} { display: none !important; }`;
         document.head.appendChild(style);
 
+        console.log('[ytspoilervoider] applied styles');
+        return true;
+    }
+
+    const hideUpcoming = () => {
+        if (CONF_HIDE_UPCOMING_STREAMS) {
+            const headings = document.querySelectorAll('#meta:has(#buttons:not(:empty)) #video-title');
+            for (const heading of headings) {
+                heading.textContent = '[Obfuscated by YTSpoilerVoider]'
+            }
+            console.log('[ytspoilervoider] hid upcoming streams');
+        }
         return true;
     }
 
@@ -40,6 +56,17 @@
         const video = document.querySelector('.html5-main-video');
         if (!video || !Number.isFinite(video.currentTime)) return;
         video.currentTime = video.currentTime + amount;
+
+        if (CONF_KEEP_TIME_IN_URL) {
+            const totalSecs = video.currentTime;
+            const sec = parseInt(totalSecs % 60);
+            const totalMin = parseInt(totalSecs / 60);
+            const min = parseInt(totalMin % 60);
+            const hr = parseInt(totalMin / 60)
+            const url = URL.parse(window.location.href);
+            url.searchParams.set("t", `${hr}h${min}m${sec}s`);
+            history.replaceState(null, null, url.href);
+        }
     };
 
     const createSeekComponent = (text, duration, container) => {
@@ -57,8 +84,6 @@
     }
 
     const decorateVideoPlayer = () => {
-        console.log('applying ytspoilervoider decorations')
-
         // if (!window.location.pathname.startsWith('/watch')) return true; // no need to decorate if we aren't watching a video
 
         const durationElement = document.querySelector('.ytp-time-duration');
@@ -70,6 +95,7 @@
             createSeekComponent('+1 min', 60, container);
             createSeekComponent('+10 min', 600, container);
 
+            console.log('[ytspoilervoider] applied decorations');
             return true;
         }
         return false;
@@ -83,8 +109,12 @@
         if (!window[DECORATED_KEY]) {
             window[DECORATED_KEY] = decorateVideoPlayer();
         }
+        if (!window[OBFUSCATED_KEY]) {
+            window[OBFUSCATED_KEY] = hideUpcoming();
+        }
 
-        if (window[STYLED_KEY] && window[DECORATED_KEY]) {
+        if (window[STYLED_KEY] && window[DECORATED_KEY] && window[OBFUSCATED_KEY]) {
+            console.log('[ytspoilervoider] success!');
             observer.disconnect(); // we have done all we need to do and can stop observing
         }
     };
@@ -93,3 +123,4 @@
     const observer = new MutationObserver(activate);
     observer.observe(document.body, { childList: true, subtree: true });
 })();
+
